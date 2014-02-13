3e4e1451dd8bd3de56c5e161c199e34dc3f5af2e
Matt Sheehan <saltykracka@gmail.com>
2014-02-12 23:21:50 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,82 @@
+var net = require('net');
+var Socket = require('./socket');
+
+
+var server = net.createServer(function(conn)
+{
+	//Create New Socket Class
+	var socket = new Socket();
+
+	conn.setEncoding('utf8');
+
+
+	//Prompt to enter data.
+	conn.write('IRC Client : v.0.0.1\r\n'
+		+ 'Enter NickName: '
+		);
+
+
+	//When data is received.
+	//First data sent is name. Next Server. Next Channel. After that messages.
+	conn.on('data', function(data)
+	{
+		if(data == '\r\n')
+		{
+			if(!socket.nickname)
+			{
+				socket.nickname = socket.buffer;
+
+				conn.write('Enter Site: ');
+			}
+
+			else if(!socket.site)
+			{
+				socket.site = socket.buffer;
+
+				conn.write('Enter Channel: ');
+			}
+
+			else if(!socket.channel)
+			{
+				socket.channel = socket.buffer;
+
+				conn.write('Connecting...\r\n');
+
+				socket.connect(conn);
+			}
+			else
+			{
+				socket.sendMessage();
+			}
+
+			socket.clearBuffer();
+		}
+		else
+		{
+			socket.buffer += data;
+		}
+	});
+
+
+	//When socket is closed
+	conn.on('close', function()
+	{
+		console.log(socket.nickname + ' closed.');
+		socket.open = false;
+		socket.socket.end();
+	});
+
+
+	//Poor error handling
+	conn.on('error', function(err)
+	{
+		//swallow errors
+	});
+
+});
+
+//Start listening
+server.listen(23, function()
+{
+	console.log('\033[96mServer listening on *:23\033[39m');
+})
---
+++ socket.js
@@ -0,0 +1,110 @@
+module.exports = Socket;
+
+//Include net module
+var net = require('net');
+
+
+//Socket class
+function Socket() 
+{
+	this.nickname = '';
+	this.site = '';
+	this.channel = '';
+
+	this.buffer = '';
+
+	this.open = false;
+
+	this.socket;
+	this.connection;
+}
+
+
+//Connects socket to irc server
+Socket.prototype.connect = function(conn)
+{
+	//Create net::socket
+	var sock = net.Socket();
+
+	this.connection = conn;
+	this.socket = sock.connect(6667, this.site);
+
+	this.socket.setKeepAlive(true);
+
+	this.socket.setEncoding('utf8');
+
+
+	//Start recursive pings.
+	this.keepAlive();
+
+
+	//When first connected.
+	this.socket.on('connect', function()
+	{
+		this.socket.write('NICK ' + this.nickname + '\r\n');
+		this.socket.write('USER ' + this.nickname + ' 0 * :realname\r\n');
+		this.socket.write('JOIN  ' + this.channel + '\r\n');
+
+		this.open = true;
+
+	}.bind(this));
+
+
+	//When data is received.
+	//Write to connection.
+	this.socket.on('data', function(data)
+	{
+		this.connection.write(data + '\r\n');
+
+	}.bind(this));
+
+
+	//When socket is closed.
+	this.socket.on('close', function(err)
+	{
+		this.open = false;
+		console.log('socket closed');
+		this.connection.write('Socket closed. Restart and Reconnect.\r\n');
+
+	}.bind(this));
+
+
+	//Poor error checking
+	this.socket.on('error', function(err)
+	{
+		//swallow errors
+	});
+}
+
+
+//Sends public message to channel
+Socket.prototype.sendMessage = function()
+{
+	if(this.open)
+		this.socket.write('PRIVMSG ' + this.channel + ' :' + this.buffer + '\r\n');
+}
+
+
+//Clears message buffer
+Socket.prototype.clearBuffer = function()
+{
+	this.buffer = '';
+}
+
+
+//Sends pings as long as socket is open.
+//Recursively calls itself until the socket is closed.
+Socket.prototype.keepAlive = function()
+{
+	setTimeout(function()
+	{
+
+		if(this.open)
+		{
+			this.socket.write('PING ' + this.socket.localAddress + '\r\n');
+
+			this.keepAlive();
+		}
+
+	}.bind(this), 60000);
+}
